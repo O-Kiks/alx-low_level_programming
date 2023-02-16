c08c61123b848760e56065a334569ca521991806
O-Kiks <samuel.o.raji@gmail.com>
2023-02-16 14:53:14 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description - practicing the printf function
+ * return - 0 if successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
