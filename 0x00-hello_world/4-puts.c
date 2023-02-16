4cc9ed657f70ae4c48fc0b29574e61d9d28f6895
O-Kiks <samuel.o.raji@gmail.com>
2023-02-16 14:31:54 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-//Hello puts
+
+/*
+ * This program is to test the use
+ * of certain functions*/
+
 int main(void)
-	{
-	puts(\"Programming is like building a multilingual puzzle");	
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
