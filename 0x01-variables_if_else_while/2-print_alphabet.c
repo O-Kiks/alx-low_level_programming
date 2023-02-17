3bc9fe1a9a769daf05d3b84908b952fb62187c07
O-Kiks <samuel.o.raji@gmail.com>
2023-02-17 13:54:17 -0800

    a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ * Description: A program to prints the alpabets a-z to the standard output
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char letter= 'a';
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+	putchar('\n');
+	return(0);
+}
