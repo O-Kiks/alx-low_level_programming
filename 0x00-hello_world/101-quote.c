e32c7369bc50dddbcfe21473a1f80933336ceb65
O-Kiks <samuel.o.raji@gmail.com>
2023-02-16 16:15:34 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that pi\
-			ece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that pi
+		ece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
