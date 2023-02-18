35eb7af2d550c72e8f9d722055128e238c2849dd
O-Kiks <samuel.o.raji@gmail.com>
2023-02-17 17:38:57 -0800

    print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a pogram that forms two digit numbers
+ * from single digit numbers and prevents repetition of digits
+ * by printing lower value number
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 10; i++)
+	{
+	for (j = i + 1; j < 10; j++)
+	{
+	putchar (i + '0');
+	putchar (j + '0');
+	if (i < 8)
+	{
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	putchar ('\n');
+	return 0;
+}
