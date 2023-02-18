fc2de307652edd88ccd95957a2b4b17722d046ea
O-Kiks <samuel.o.raji@gmail.com>
2023-02-17 16:27:55 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all the hexadecimal numbers in lower case letters
+ *
+ * Return: 0 Alwyas (success)
+ */
+
+int main(void)
+{
+	char num = 48;
+	char letter = 'a';
+
+	while (num < 58)
+	{
+	putchar (num);
+	num++;
+	}
+	
+	
+	while (letter <= 'f')
+	{
+	putchar (letter);
+	letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
