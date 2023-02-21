7e2e109c5ab922a4e74185dce2cd622612519f71
O-Kiks <samuel.o.raji@gmail.com>
2023-02-21 14:12:15 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints _putchar followed by a new line
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
