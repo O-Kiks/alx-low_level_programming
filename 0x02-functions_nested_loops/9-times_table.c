fd2429d0ac40a1510f2c8e14d1b44387213b88cf
O-Kiks <samuel.o.raji@gmail.com>
2023-02-22 13:43:34 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,18 +13,16 @@ void times_table(void)
 {
 	int number = 0;
 	int count = 0;
-	
 	for (count = 0; count <= 9; count++)
 	{
 		for (number = 0; number <= 9; number++)
 		{
 			int product = count * number;
 			int dive = product / 10;
-			int mode = product % 10;
-			
+			int mode = product % 10;	
 			if (product < 10)
 			{
-				_putchar('0'+ product);
+				_putchar('0' + product);
 
 				if (number == 9)
 				{
@@ -32,11 +30,12 @@ void times_table(void)
 				}
 				_putchar (',');
 				_putchar (' ');
+				_putchar (' ');
 			}
 			else
 			{
-				_putchar ('0'+ dive);
-				_putchar ('0'+ mode);
+				_putchar ('0' + dive);
+				_putchar ('0' + mode);
 				
 				if (number == 9)
 				{
