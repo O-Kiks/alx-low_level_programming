231b5c882f74f66905de72cd99b57b474e1b3a22
O-Kiks <samuel.o.raji@gmail.com>
2023-02-21 18:50:01 -0800

    Modular

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,28 +1,21 @@
 #include "main.h"
 
 /**
- * print_last_digit - Entry point
- *
- * @modular: prints last digit of a number
- *
- * Return: Last Digit if true.
- */
+ *  * print_last_digit - main
+ *   * @n: parameter
+ *    * Return: last value of n
+ *     */
 
-int print_last_digit(int modular)
+int print_last_digit(int n)
 {
-	
-	if (modular < 0)
-	{
-		modular = -modular;
-		char mode = modular % 10;
-		
-		_putchar ('0' + mode);
-	return (modular % 10);
-	}
-	else
-	{
-		mode = modular;
-		_putchar ('0'+ mode);
-		return (modular % 10);
-	}
+		int read = n % 10;
+
+			if (read < 0)
+					{
+								read *= -1;
+									}
+
+				_putchar (read + '0');
+
+					return (read);
 }
