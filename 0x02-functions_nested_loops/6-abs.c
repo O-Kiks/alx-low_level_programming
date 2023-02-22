a84bbb64540fbe71a85cf6512363e8ec02b6500e
O-Kiks <samuel.o.raji@gmail.com>
2023-02-21 18:02:31 -0800

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @n: integer value received
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	int n1 == n * -1;
+	int n2 == n * 1;
+
+	if (n >= 0)
+	{
+		putchar(n2);
+	}
+	else
+	{
+		putchar(n1);
+	}
+	putchar('\n');
+	return (0);
+}
