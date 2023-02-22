e846ea34a9d907d4604c042cba57aa2a746d032b
O-Kiks <samuel.o.raji@gmail.com>
2023-02-21 18:53:22 -0800

    Modular

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,18 +4,16 @@
  *  * print_last_digit - main
  *   * @n: parameter
  *    * Return: last value of n
- *     */
+ */
 
 int print_last_digit(int n)
 {
 		int read = n % 10;
 
-			if (read < 0)
-					{
-								read *= -1;
-									}
-
-				_putchar (read + '0');
-
-					return (read);
+		if (read < 0)
+		{
+			read *= -1;
+		}
+		_putchar (read + '0');
+		return (read);
 }
