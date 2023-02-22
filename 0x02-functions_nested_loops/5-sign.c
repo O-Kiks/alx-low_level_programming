278051b3620b61bb78370c9b7f1a71ea27c747d0
O-Kiks <samuel.o.raji@gmail.com>
2023-02-21 17:46:06 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,14 +5,15 @@
  *
  * @n: received integer
  *
- * Return: 1 if n>0; 0 if n==0; -1 if n<0.
+ * Return: 1 if n is greater than 0; 0 if n equals 0
+ * and -1 if n is less than 0.
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar (45);
+		_putchar (43);
 		return (1);
 	}
 	else if (n == 0)
