b3de5f85b054e94b66e0f162013eac960bce343e
O-Kiks <samuel.o.raji@gmail.com>
2023-02-21 18:05:23 -0800

    Absolute values

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,17 +10,20 @@
 
 int _abs(int n)
 {
-	int n1 == n * -1;
-	int n2 == n * 1;
+	int n1;
+	int n2;
+
+	n1 == -1 * n;
+	n2 == 1 * n;
 
 	if (n >= 0)
 	{
-		putchar(n2);
+		_putchar(n2);
 	}
 	else
 	{
-		putchar(n1);
+		_putchar(n1);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
