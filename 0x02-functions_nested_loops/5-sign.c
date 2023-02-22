53a904d46f59ca4278e7632aba7eefa35610bfd5
O-Kiks <samuel.o.raji@gmail.com>
2023-02-21 17:36:43 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,16 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		putchar (45);
+		_putchar (45);
 	}
 	else if (n == 0)
 	{
 		return (0);
-		putchar (48);
+		_putchar (48);
 	}
 	else
 	{
 		return (-1);
-		putchar (45);
+		_putchar (45);
 	}
 }
