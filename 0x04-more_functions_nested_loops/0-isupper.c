4d6f0a174ab72e8fe6102f1a3237d9cadde53018
O-Kiks <samuel.o.raji@gmail.com>
2023-02-23 09:18:35 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * description: a function that checks for a digit (0 through 9)
+ *
+ * Return: 1 if c is uppercase
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+	{
+		return (1);
+	}
+	return (0);
+}
