c6dde9fa69f2781313ea5faa340dafdc12194183
O-Kiks <samuel.o.raji@gmail.com>
2023-02-23 10:54:26 -0800

    Print digits from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: print digits from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit <=9; digit++)
+	{
+		_putchar(digit + '0');
+	}
+	_putchar ('\n');
+}
