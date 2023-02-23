ef1a5059fd55e154f421b6acd99e29c7aa08338d
O-Kiks <samuel.o.raji@gmail.com>
2023-02-23 10:43:15 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: argument to be checked
+ * @b: second argument to be checked
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
