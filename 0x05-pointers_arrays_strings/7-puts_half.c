c0c7c4f0240f515609dd7f4908369ae5c2760199
O-Kiks <samuel.o.raji@gmail.com>
2023-02-27 14:00:33 -0800

    print only last half characters

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - entry point
+ *
+ * @str: input string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int count, i;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	if (count % 2 == 1)
+	{
+		i = count / 2;
+	}
+	else
+	{
+		i = (count - 1) / 2;
+	}
+
+	for (i++; i < count; i++)
+	{
+		_putchar (str[i]);
+	}
+	_putchar ('\n');
+}
