c4629c88640e74f4c7fa7afe9ba2e523707a7ef1
O-Kiks <samuel.o.raji@gmail.com>
2023-02-27 11:17:21 -0800

    print spring in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - entry point
+ *
+ * @s: input string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (count == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+
+	for (count--, count >= 0; count--)
+	{
+		_putchar (s[count]);
+	}
+	_putchar ('\n');
+}	
