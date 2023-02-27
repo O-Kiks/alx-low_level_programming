5e82c3f920226b5ecb0f2cd8d5f7d5072a2bfe13
O-Kiks <samuel.o.raji@gmail.com>
2023-02-27 10:16:14 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ *
+ * @s: input variable
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (*(s + count) != '\0')
+	{
+		count++
+	}
+	return (count);
+}
