0f4146ccc3ca1ec5fdf308c5d8ca3f2a17ff8385
O-Kiks <samuel.o.raji@gmail.com>
2023-03-02 15:15:15 -0800

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,8 +3,8 @@
 /**
  * _strcmp - entry point
  *
- * @*s1: value of string 1
- * @*s2: vaue of string 2
+ * @s1: value of string 1
+ * @s2: vaue of string 2
  *
  * Return: return an integer depending on difference
  * between compared strings
