0cc8bd5a62c62795cc356536f0cd191d691c32df
O-Kiks <samuel.o.raji@gmail.com>
2023-03-08 13:16:46 -0800

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int is_palindrome - entry point
+ *
+ * @s: input string
+ *
+ * Return: 0 or 1
+ *
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
